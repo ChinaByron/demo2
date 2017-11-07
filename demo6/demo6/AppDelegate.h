156d6d87ee8388f9d31467233f5fe1966c497f6d
hongyuan.tian <hongyuan.tian@hifivetech.com>
2017-11-07 14:30:31 +0800

    ceshi6

---
+++ demo6/demo6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo6
+//
+//  Created by Byron on 2017/11/7.
+//  Copyright © 2017年 Byron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo6/demo6/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  demo6
+//
+//  Created by Byron on 2017/11/7.
+//  Copyright © 2017年 Byron. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

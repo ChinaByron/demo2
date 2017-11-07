53c4547455951a54fd85a0d48ba2a8882da733d8
hongyuan.tian <hongyuan.tian@hifivetech.com>
2017-11-07 14:44:02 +0800

    ceshi

---
+++ demo6/demo6/class/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  demo6
+//
+//  Created by Byron on 2017/11/7.
+//  Copyright © 2017年 Byron. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+- (void)test;
+@end
---
+++ demo6/demo6/class/test.m
@@ -0,0 +1,15 @@
+//
+//  test.m
+//  demo6
+//
+//  Created by Byron on 2017/11/7.
+//  Copyright © 2017年 Byron. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+- (void)test {
+    NSLog(@"ddd");
+}
+@end
